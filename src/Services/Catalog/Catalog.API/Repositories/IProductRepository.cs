94dbc639de2e62a6912c21ae03a9787b3e930dcd
Gabriel Carmelo <Gabriel.Carmelo@pe.ey.com>
2022-10-23 19:48:02 -0500

    Discout grpc service done

--- src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -4,7 +4,7 @@ namespace Catalog.API.Repositories
 {
     public interface IProductRepository
     {
-         Task<IEnumerable<Product>> GetProducts();
+        Task<IEnumerable<Product>> GetProducts();
         Task<Product> GetProduct(string id);
         Task<IEnumerable<Product>> GetProductByName(string name);
         Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
---
+++ src/Services/Discount/Discount.API/Program.cs
@@ -0,0 +1,33 @@
+using Discount.API.Entities;
+using Discount.API.Extensions;
+using Discount.API.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// app.MigrateDatabase();
+
+app.Run();
